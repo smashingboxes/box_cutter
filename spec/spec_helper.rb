7a67ff5eb23f0385a4eeb459a6cdaf7e2662c5ce
BrandonMathis <BeMathis@gmail.com>
2014-08-09 14:49:14 -0400

    Generated Gemfile with pg as default database gem
    Generate standard rails gemfile
    
    Extract gem definitions into seperate model
    
    Correct spec title
    
    Refactor how Gemfile is defined
    
    There is really no need to follow the way rails generated the Gemfile is
    there?
    
    Generate Gemfile with postgresql as default db gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,15 +7,23 @@ require (Pathname.new(__FILE__).dirname + '../lib/box_cutter').expand_path
 
 Dir['./spec/support/**/*.rb'].each { |file| require file }
 
+
 RSpec.configure do |config|
   config.include BoxCutterTestHelpers
 
   config.before(:all) do
+    ENV['DISABLE_SPRING'] = 'true'
+    remove_project_directory
     create_tmp_directory
+    cut_the_box
   end
 
   config.before(:each) do
+    ENV['DISABLE_SPRING'] = 'true'
     drop_dummy_database
+  end
+
+  config.after(:all) do
     remove_project_directory
   end
 end
